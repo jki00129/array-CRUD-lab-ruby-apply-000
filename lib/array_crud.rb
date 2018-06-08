b3bb35aa87715d0e869ddcc402423a30c5db1432
Jeewon Kim <jki00129@gmail.com>
2018-06-08 15:47:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  Array.new(4) { Hash.new } #=> [{1}, {2}, {3}, {4}]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr << 5    #=> [1, 2, 3, 4, 5]
 end
 
 def add_element_to_start_of_array(array, element)
